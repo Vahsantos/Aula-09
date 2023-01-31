3063709e0dd83e31632a60ab47cdee70f7cda213
Gabriel Lima <gabriel.lyma16@gmail.com>
2023-01-31 20:43:09 -0300

    first commit

---
+++ exercicios/ex1.js
@@ -0,0 +1,7 @@
+let colunas = ['jin', 'jhope','namjoon']
+
+let numeros = [[2,0,0],[2,1,2], [2,2,4],[2,3,6],[2,4,8],[2,5,10],[2,6,12],[2,7,14],[2,8,16],[2,9,18],[2,10,20]]
+
+alert('É a taboada do 2')
+
+alert('Observei que era uma sequencia númerica,2x0 = 0 e assim or diante')
---
+++ script.js
@@ -0,0 +1,17 @@
+let alunos =  ['Charles', 'Fernando','Lucas','Danilo' ]//array dp tipo vetor
+console.log(alunos[0])
+console.log(alunos[1])
+console.log(alunos[2])
+
+let notas =[[9,8,10],[5,4,10], [10,9,8]]// arrau tipo matriz
+console.log(notas)
+console.log('Primeira nota da primeira linha', notas[0][0])
+console.log('Ultima linha e ultima coluna',notas[2] [2])
+// adicionando uma linha na matriz
+notas.push([10,10,10])
+console.log(notas)
+
+/*
+3x3
+matriz [3][3]
+*/
